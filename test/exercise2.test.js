6273aa2d64be47d33c83f06f61103571202f274a
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-03-02 09:52:19 +0100

    Exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -1,11 +1,12 @@
 const expect = require('chai').expect;
 const program = require('../src/exercise2');
 
-describe(`${program.title}`, function () {
+describe.only(`${program.title}`, function () {
 
   it('Function yolo with mysterious parameter should return a list of messages', function () {
 
-    const whereIsMyMind = "???";
+    const whereIsMyMind = [{name: "William", sex:1}, {name: "Gaius", sex:1}, {name: "Lee", sex:1}, {name: "Kara", sex:2},
+    {name: "Laura", sex:2}, {name: "", sex:9}, {name: "", sex:9}];
 
     const output = [
       "0 not known character(s) ",
